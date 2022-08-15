15a64cc704440107e7c16fe633eeb1a98566e97c
“Ajay <ajay.ip3105@gmail.com>
2022-08-14 21:38:00 -0400

    Accomplish task

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,16 @@
+class PlayersController < ApplicationController
+    get '/players' do
+        sortedPlayers = Player.playerList
+        sortedPlayers.to_json
+    end
+
+    post '/players' do
+        player = Player.create(params)
+        player.to_json
+    end
+
+    delete '/players/:id' do
+        player = Player.find_by(id: params[:id])
+        player.destroy
+    end
+end
